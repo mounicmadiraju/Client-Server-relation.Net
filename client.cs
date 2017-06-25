ee27cab95c5f9e77d0f3cda2627661cb6dcd7f34
Mounic Madiraju <mounicraju@gmail.com>
2017-06-25 14:47:30 +0800

    Create client.cs

---
+++ client.cs
@@ -0,0 +1,44 @@
+//CLIENT PROGRAM
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+ 
+namespace Client336
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting.....");
+                tcpclnt.Connect("10.18.227.162", 8001);                
+                Console.WriteLine("Connected");
+                Console.Write("Enter the string to be transmitted : ");
+                String str = Console.ReadLine();
+                Stream stm = tcpclnt.GetStream();
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting.....");
+                stm.Write(ba, 0, ba.Length);
+                byte[] bb = new byte[100];
+                int k = stm.Read(bb, 0, 100);
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+                tcpclnt.Close();
+                Console.Read();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }
+    }
+}
